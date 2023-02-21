320289bc9686b15ae97beeb59a7cb84bb904130d
Kenny <khuynh2@student.fitchburgstate.edu>
2023-02-21 07:42:58 -0500

    Slingshot test
    Created the code for pulling the slingshot back, still working on making it release the pellet

---
+++ Assets/DevSripts/Slingshot.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public LineRenderer[] lineRenderers;
+    public Transform[] stripPositions;
+    public Transform center;
+    public Transform idlePosition;
+    public Vector3 currentPosition;
+    public float maxLength;
+    bool isMouseDown;
+
+    public GameObject pelletPrefab;
+    public float pelletPositionOffset;
+
+    Rigidbody2D pellet;
+    Collider2D pelletCollider;
+    // Start is called before the first frame update
+    // Sets the position of the line strips
+    void Start()
+    {
+        lineRenderers[0].positionCount = 2;
+        lineRenderers[1].positionCount = 2;
+        lineRenderers[0].SetPosition(0, stripPositions[0].position);
+        lineRenderers[1].SetPosition(0, stripPositions[1].position);
+
+        CreatePellet();
+    }
+
+    void CreatePellet()
+    {
+        pellet = Instantiate(pelletPrefab).GetComponent<Rigidbody2D>();
+        pelletCollider = pellet.GetComponent<Collider2D>();
+        pelletCollider.enabled = false;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Holding mouse on the strip and drag
+        if (isMouseDown)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10;
+
+            currentPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            currentPosition = center.position + Vector3.ClampMagnitude(currentPosition - center.position, maxLength); // This creates a limited distance of how far you can drag the strip
+            SetStrips(currentPosition);
+        }
+        else
+        {
+            ResetStrips();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isMouseDown = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isMouseDown = false;
+    }
+
+    void ResetStrips()
+    {
+        currentPosition = idlePosition.position;
+        SetStrips(currentPosition);
+    }
+
+    void SetStrips(Vector3 position)
+    {
+        lineRenderers[0].SetPosition(1, position);
+        lineRenderers[1].SetPosition(1, position);
+
+
+        if(pellet)
+        {
+            Vector3 dir = position - center.position;
+            pellet.transform.position = position + dir.normalized * pelletPositionOffset;
+        }
+    }
+}
