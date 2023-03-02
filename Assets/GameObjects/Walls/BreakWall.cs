91faa36927d3425929992a75d3e1570f535248e0
vacolavita <vacolavita@gmail.com>
2023-03-02 11:33:32 -0500

    New wall types, better collision

--- Assets/GameObjects/Walls/BreakWall.cs
+++ Assets/GameObjects/Walls/BreakWall.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class BreakWall : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -16,7 +17,13 @@ public class BreakWall : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        if (collision.gameObject.CompareTag("Sticky"))
+        {
+            Debug.Log("Sticky");
+            rb.velocity = new Vector2(rb.velocity.x * 0.6f, rb.velocity.y * 0.6f);
+        }
     }
+
 
     // Update is called once per frame
     void Update()
